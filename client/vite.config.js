d3d2b17b18afd705a167836981a00a142b219f5c
Michael Loomis <85528807+labordude@users.noreply.github.com>
2023-06-17 22:27:22 -0500

    updating config

--- client/vite.config.js
+++ client/vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
         target: "http://127.0.0.1:5555",
         changeOrigin: true,
         secure: false,
+        rewrite: path => path.replace(/^\/edit/, ""),
       },
     },
   },
