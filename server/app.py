34179bd22cbef24164f0c1c76b2c877453c49e44
valeria caliguire <caliguireval@gmail.com>
2023-06-21 11:49:48 -0600

    reservation_date error, otherwise, complete

--- server/app.py
+++ server/app.py
@@ -33,6 +33,157 @@ api = Api(app)
 def home():
     return ""
 
+#GET /customers
+class Customers(Resource):
+    def get(self):
+        #1. query
+        customers = Customer.query.all()
+        #2. dict
+        customers_dict = [c.to_dict() for c in customers]
+        #3. res return
+        res = make_response(
+            customers_dict,
+            200
+        )
+        return res 
+#4. api
+api.add_resource(Customers, "/customers")
+
+#GET /customers/int:id
+class OneCustomer(Resource):
+    def get(self, id):
+        try:
+            customer = Customer.query.filter_by(id=id).first()
+            customer_dict = customer.to_dict()
+            res = make_response(
+                customer_dict,
+                200
+            )
+            return res
+        except:
+            return {"error": "404: Customer not found"}, 404
+api.add_resource(OneCustomer, "/customers/<int:id>")
+
+
+#POST /customer
+class NewCustomer(Resource):
+    def get(self):
+        customers = Customer.query.all()
+        customers_dict = [c.to_dict() for c in customers]
+        res = make_response(
+            customers_dict,
+            200
+        )
+        return res
+#POST
+    def post(self):
+        data = request.get_json()
+        try:
+            new_customer = Customer(
+                name= data.get("name"),
+                email= data.get("email")
+            )
+            db.session.add(new_customer)
+            db.session.commit()
+
+            new_customer_dict = new_customer.to_dict()
+
+            res = make_response(
+                new_customer_dict,
+                201
+            )
+            return res
+        except:
+            return {"error": "400: Validation error"}, 400
+api.add_resource(NewCustomer, "/customer")
+
+
+#GET /locations
+class Locations(Resource):
+    def get(self):
+        locations = Location.query.all()
+        locations_dict = [l.to_dict() for l in locations]
+        res = make_response(
+            locations_dict,
+            200
+        )
+        return res
+
+api.add_resource(Locations, "/locations")
+
+
+#POST /reservations
+class Reservations(Resource):
+    def get(self):
+        reservations = Reservation.query.all()
+        reservations_dict = [r.to_dict() for r in reservations]
+        res = make_response(
+            reservations_dict,
+            200
+        )
+        return res
+#POST
+    def post(self):
+        data = request.get_json()
+        try:
+            new_reservation = Reservation(
+                party_name=data.get("party_name"),
+                party_size=data.get("party_size"),
+                reservation_date=data.get("reservation_date"),
+                location_id=data.get("location_id"),
+                customer_id=data.get("customer_id")
+            )
+            db.session.add(new_reservation)
+            db.session.commit()
+
+            new_reservation_dict= new_reservation.to_dict()
+
+            res = make_response(
+                new_reservation_dict,
+                201
+            )
+            return res
+        except:
+            return {"error": "400: Validation error"}, 400
+
+api.add_resource(Reservations, "/reservations")
+
+
+#PATCH /reservations/int:id
+class OneReservation(Resource):
+    def get(self, id):
+        reservation = Reservation.query.filter_by(id=id).first()
+        reservation_dict = reservation.to_dict()
+        res = make_response(
+            reservation_dict,
+            200
+        )
+        return res
+#PATCH
+    def patch(self, id):
+        try:
+            reservation = Reservation.query.filter_by(id=id).first()
+        except:
+            return {"error": "404: Reservation not found"}, 404
+        data = request.get_json()
+        for attr in data:
+            setattr(reservation, attr, data.get(attr))
+        db.session.add(reservation)
+        db.session.commit()
+        return make_response(reservation.to_dict(), 200)
+
+#DELETE /reservations/int:id
+    def delete(self, id):
+        reservation = Reservation.query.filter_by(id=id).first()
+        db.session.delete(reservation)
+        db.session.commit()
+
+        return make_response({}, 204)
+
+api.add_resource(OneReservation, "/reservation/<int:id>")
+
+
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -18,19 +18,144 @@ metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer:
+class Customer(db.Model, SerializerMixin):
     __tablename__ = "customers"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
 
 
-class Location:
+    #RELATIONSHIPS
+    #A Customer has many Reservations
+    reservations_of_cur_customer = db.relationship("Reservation", back_populates="customer")
+
+
+    #ASSOCIATION PROXY
+    #A Customer has been to many Locations through Reservations
+    locations_of_cur_customer = association_proxy("reservations_of_cur_customer", "location")
+
+
+    #SERIALIZE RULES
+    serializer_rules = (
+        "-locations_of_cur_customer.customers_of_cur_location",
+        "-reservations_of_cur_customer.customer",
+        "-reservations_of_cur_customer.location"
+        )
+
+
+    #VALIDATIONS
+    @validates("name")
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Input must have a name.")
+        return name
+    
+    #####REVIEW THIS SYNTAX#####
+    @validates("email")
+    def validate_email(self, key, email):
+        emails = db.session.query(Customer.email).all()
+        if email in emails:
+            raise ValueError("Must be a unique email.")
+        return email
+
+
+
+
+
+class Location(db.Model, SerializerMixin):
     __tablename__ = "locations"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    max_party_size = db.Column(db.Integer, nullable=False)
+
+    #RELATIONSHIPS
+    #A Location has many Reservations
+    reservations_of_cur_location = db.relationship("Reservation", back_populates="location")
 
 
-class Reservation:
+    #ASSOCIATION PROXY
+    #A Location has many Customers through Reservations
+    customers_of_cur_location = association_proxy("reservations_of_cur_location", "customer")
+
+
+    #SERIALIZE RULES
+    serialize_rules = (
+        "-customers_of_cur_location.location",
+        "-reservations_of_cur_location.location",
+    )
+
+
+    #VALIDATIONS
+    @validates("name")
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Input must have a name.")
+        return name
+    
+    @validates("max_party_size")
+    def validate_max_party_size(self, key, max_party_size):
+        if not max_party_size:
+            raise ValueError("Input must have a max party size.")
+        return max_party_size
+
+
+
+
+class Reservation(db.Model, SerializerMixin):
     __tablename__ = "reservations"
 
     id = db.Column(db.Integer, primary_key=True)
+    party_name = db.Column(db.String)
+    party_size = db.Column(db.Integer)
+    reservation_date = db.Column(db.Date, nullable=False)
+    #reservation_date = db.Column(db.DateTime, server_default=db.func().now())
+    #FOREIGN KEYS
+    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"))
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
+
+    #RELATIONSHIPS
+    #A Reservation belongs to a Customer
+    customer = db.relationship("Customer", back_populates="reservations_of_cur_customer")
+    #reservations_of_cur_customer = db.relationship("Reservation", back_populates="customer")
+
+    #A Reservation belongs to a Customer and a Location
+    location = db.relationship("Location", back_populates="reservations_of_cur_location")
+    #reservations_of_cur_location = db.relationship("Reservation", back_populates="location")
+
+
+    #SERIALIZE RULES
+    serialize_rules = (
+        "-location.reservations_of_cur_location",
+        "-customer.reservations_of_cur_customer"
+    )
+
+
+    #VALIDATIONS
+    @validates("reservation_date")
+    def validate_reservation_date(self, key, reservation_date):
+        if not reservation_date:
+            raise ValueError("Input must have a reservation date.")
+        return reservation_date
+
+    @validates("party_name")
+    def validate_party_name(self, key, party_name):
+        if not party_name:
+            raise ValueError("Input must have a party name.")
+        return party_name
+
+    @validates("customer_id")
+    def validate_customer_id(self, key, customer_id):
+        if not customer_id:
+            raise ValueError("Input must have a customer id.")
+        return customer_id
+
+    @validates("location_id")
+    def validate_location_id(self, key, location_id):
+        if not location_id:
+            raise ValueError("Input must have a location id.")
+        return location_id
+
+        #?????????????
+        #a customer cannot have more than one reservation for the same date and location
